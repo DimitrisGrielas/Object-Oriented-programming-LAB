5f5d9681e61e49b4d0a9fd5850edb4056946a936
DimitrisGrielas <dimitrisgkrielas@gmail.com>
2024-11-01 17:56:30 +0200

    Class test and exercises

--- Theory/Classes_test.cpp
+++ Theory/Classes_test.cpp
@@ -27,44 +27,68 @@ class Rectangle
             cout << "Deconstructed\n";
         }
 
-        inline void setWidth(int w)
-        {
-            width = w;
-        }
+        inline void setWidth(int w);
 
-        inline void setHeight(int h)
-        {
-            height = h;
-        }
+        inline void setHeight(int h);
 
-        inline int getWidth() const
-        {
-            return width;
-        }
+        inline int getWidth() const;
 
-        inline int getHeight() const
-        {
-            return height;
-        }
+        inline int getHeight() const;
 
-        inline int getEmvado() const
-        {
-            int emv;
-            emv = width*height;
-            return emv;
-        }
+        inline int getEmvado() const;
 
-        inline void print_rect()
-        {
-            cout << "Rectangle width = " << getWidth() << "\nRectangle height = " << getHeight() << "\n";
-        }
+        inline void print_rect();
 
-        inline void print_emv()
-        {
-            cout << "Rectangle emvado = " << getEmvado() <<"\n";
-        }
+        inline void print_emv();
+
 };
 
+inline void Rectangle::setWidth(int w)
+{
+    width = w;
+}
+
+inline void Rectangle::setHeight(int h)
+{
+    height = h;
+}
+
+inline int Rectangle::getWidth() const
+{
+    return width;
+}
+
+inline int Rectangle::getHeight() const
+{
+    return height;
+}
+
+inline int Rectangle::getEmvado() const
+{
+    return getWidth()*getHeight();
+}
+
+inline void Rectangle::print_rect()
+{
+    cout << "Rectangle width = " << getWidth() << "\nRectangle height = " << getHeight() << "\n";
+}
+
+inline void Rectangle::print_emv()
+{
+    cout << "Rectangle emvado = " << getEmvado() <<"\n";
+}
+
+inline void inc_rect(Rectangle &r)
+{
+    r.setWidth(r.getWidth() + 1);
+    r.setHeight(r.getHeight() + 1);
+}
+
+inline Rectangle add_rect(Rectangle &r1, Rectangle &r2)
+{
+    Rectangle r (r1.getWidth()+r2.getWidth(), r1.getHeight()+r2.getHeight());
+    return r;
+}
 
 int main()
 {
@@ -78,10 +102,15 @@ int main()
     //r.setHeight(height);
     Rectangle r1;
     Rectangle r2 (width, height);
+    Rectangle r3;
     r1.print_rect();
     r1.print_emv();
     r2.print_rect();
     r2.print_emv();
+    inc_rect(r1);
+    r1.print_rect();
+    r3 = add_rect(r1, r2);
+    r3.print_rect();
     
     return 0;
 }
