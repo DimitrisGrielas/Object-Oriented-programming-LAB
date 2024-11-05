e21f8aa60d499b37da83d40dbcc5d2cd55ef7016
Dimitris Grielas <dimitrisgkrielas@gmail.com>
2024-11-05 18:25:36 +0200

    Lab 2 task 3

---
+++ Lab2_Tasks/lab2_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int stud_part_count;
+
+    cout << "How many students did participate in the study? ";
+    cin >> stud_part_count;
+    cout << "\n";
+
+    char *stud_names = new char[stud_part_count][stud_part_count];
+
+}
